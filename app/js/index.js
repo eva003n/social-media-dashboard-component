e0a8a1f6828e803e05c0752d39ee9f61e58e26e7
Evan ngugi wambui <evanngugi547@gmail.com>
2024-11-22 19:18:19 +0300

    created some components

---
+++ app/js/index.js
@@ -0,0 +1,30 @@
+const themeSwitcherBtn = document.querySelector(".js-theme-toggler");
+
+//get theme from storage and apply it
+const theme = localStorage.getItem("theme") || "light";
+const htmlElement = document.querySelector("html");
+htmlElement.setAttribute("data-theme", theme);
+
+themeSwitcherBtn.addEventListener("click", () => {
+    if(themeSwitcherBtn.classList.contains("active")) {
+        //light-mode
+        htmlElement.dataset.theme="light";
+         saveThemeToStorage(htmlElement.dataset.theme);
+        themeSwitcherBtn.classList.remove("active");
+        themeSwitcherBtn.classList.add("disabled");
+      
+   
+    }else {
+        //dark-mode
+        htmlElement.dataset.theme="dark";
+        //save theme to local storage
+        saveThemeToStorage(htmlElement.dataset.theme);
+        themeSwitcherBtn.classList.add("active"); 
+        themeSwitcherBtn.classList.remove("disabled"); 
+
+    }
+});
+//save to storage
+function saveThemeToStorage(theme) {
+    localStorage.setItem("theme", theme);
+}
